a238f4218408bd5f432f6eeea36ae72fce982eb4
Julius Weyl <julius.weyl@gmail.com>
2020-04-13 09:15:50 +0200

    create base layout

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  Dicee-iOS13
+//
+//  Created by Julius Weyl on 13.04.20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageOne: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
